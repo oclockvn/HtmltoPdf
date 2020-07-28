ab93a0d1bbfd199d95b31d3519ea27e8d6ac90b0
Quang Phan <oclockvn@gmail.com>
2020-07-28 14:51:46 +0700

    format value on table

--- HtmlToPdf.Web/Pages/Report.cshtml.cs
+++ HtmlToPdf.Web/Pages/Report.cshtml.cs
@@ -1,17 +1,170 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HtmlToPdf.Web.Pages
 {
     public class ReportModel : PageModel
     {
+        public List<Technology> Technologies { get; set; } = new List<Technology>();
+
         public void OnGet()
         {
+            Technologies = new List<Technology>
+            {
+                new Technology
+                {
+                    Name = "LED LIGHTING",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "VOLAGE OPTIMISATION",
+                    Sell = 159_430,
+                    Rebate = 65_445,
+                    OutOfPocket = 93_985,
+                    UsageReduction = 29,
+                    DemandReduction = 8
+                },
+                new Technology
+                {
+                    Name = "MONITORING",
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "POWER FACTOR CORRECTION",
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "SOLAR POWER",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "BATTERY ENERGY STORAGE",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+                new Technology
+                {
+                    Name = "--",
+                    Sell = 0,
+                    Rebate = 0,
+                    OutOfPocket = 0,
+                    UsageReduction = 0,
+                    DemandReduction = 0
+                },
+            };
 
         }
+    }
+
+    public class Technology
+    {
+        public string Name { get; set; }
+        public double Sell { get; set; }
+        public double Rebate { get; set; }
+        public double OutOfPocket { get; set; }
+        public double UsageReduction { get; set; }
+        public double DemandReduction { get; set; }
     }
 }
